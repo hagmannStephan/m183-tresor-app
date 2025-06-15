79c235a289250e9d20f4f721207003212e4b0d22
Hagmann Stephan <stephanhagmann@outlook.com>
2025-06-15 15:19:35 +0200

    fix: build issue with servier decleration of UserDetailsService

--- 183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/service/impl/UserDetailsServiceImpl.java
+++ 183_12_1_tresorbackend_rupe-master/src/main/java/ch/bbw/pr/tresorbackend/service/impl/UserDetailsServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Service("userDetailsServiceImpl")
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
 
