f40e0dc8acb25d84d353801a3aef1d6143db24c1
Hagmann Stephan <stephanhagmann@outlook.com>
2025-07-08 11:49:47 +0200

    enhancement: secrets panel only gets displayed if user is logged in

--- 183_12_2_tresorfrontend_rupe-master/src/pages/Layout.js
+++ 183_12_2_tresorfrontend_rupe-master/src/pages/Layout.js
@@ -1,31 +1,48 @@
 import { Outlet, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-/**
- * Layout
- * @author Peter Rutschmann
- */
-const Layout = ({loginValues}) => {
+const Layout = ({ loginValues }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        // Initialize from localStorage
+        const loggedIn = localStorage.getItem('isLoggedIn') === 'true';
+        setIsLoggedIn(loggedIn);
+    }, []);
+
+    // Update state when loginValues change
+    useEffect(() => {
+        const loggedIn = loginValues.email !== '';
+        setIsLoggedIn(loggedIn);
+        localStorage.setItem('isLoggedIn', loggedIn.toString());
+    }, [loginValues.email]);
+
     return (
         <>
             <nav>
                 <h1>The secret tresor application</h1>
-                <p>{loginValues.email === '' ? 'No user logged in' : 'user:' + loginValues.email}</p>
+                <p>{loginValues.email === '' ? 'No user logged in' : 'user: ' + loginValues.email}</p>
                 <ul>
-                    <li><a href="/">Secrets</a>
-                    <ul>
-                        <li><Link to="/secret/secrets">my secrets</Link></li>
-                        <li><Link to="/secret/newcredential">new credential</Link></li>
-                        <li><Link to="/secret/newcreditcard">new credit-card</Link></li>
-                        <li><Link to="/secret/newnote">new note</Link></li>
-                    </ul>
+                    {isLoggedIn && (
+                        <li>
+                            <a href="/">Secrets</a>
+                            <ul>
+                                <li><Link to="/secret/secrets">my secrets</Link></li>
+                                <li><Link to="/secret/newcredential">new credential</Link></li>
+                                <li><Link to="/secret/newcreditcard">new credit-card</Link></li>
+                                <li><Link to="/secret/newnote">new note</Link></li>
+                            </ul>
+                        </li>
+                    )}
+                    <li>
+                        <a href="/">User</a>
+                        <ul>
+                            <li><Link to="/user/login">login</Link></li>
+                            <li><Link to="/user/register">register</Link></li>
+                        </ul>
                     </li>
-                    <li><a href="/">User</a>
-                    <ul>
-                        <li><Link to="/user/login">login</Link></li>
-                        <li><Link to="/user/register">register</Link></li>
-                    </ul>
-                    </li>
-                    <li><a href="/">Admin</a>
+                    <li>
+                        <a href="/">Admin</a>
                         <ul>
                             <li><Link to="/user/users">All users</Link></li>
                             <li>Add user</li>
@@ -38,10 +55,10 @@ const Layout = ({loginValues}) => {
                     </li>
                 </ul>
             </nav>
-            <hr/>
-            <Outlet/>
+            <hr />
+            <Outlet />
         </>
-    )
+    );
 };
 
 export default Layout;
